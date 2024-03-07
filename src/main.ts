36427f629197c7d078ff2fa29010353919609d80
Val-d-emar <vhot@live.ru>
2024-03-07 17:21:50 +0400

    init: swagger /docs point

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import YAML = require('yamljs');
+import path = require('path'); // from 'path';
+import swaggerUi = require('swagger-ui-express'); // from 'swagger-ui-express';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Загрузка файла документации
+  const swaggerDocument = YAML.load(path.join(__dirname, '../doc', 'api.yaml'));
+  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
   await app.listen(4000);
 }
 bootstrap();
